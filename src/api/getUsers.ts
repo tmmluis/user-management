7be7a9967968a50b9b08e8d979e78998324b0224
Tiago Luis <tmmluis@gmail.com>
2023-09-11 16:41:32 +0100

    add user table

---
+++ src/api/getUsers.ts
@@ -0,0 +1,23 @@
+const BASE_URL = 'https://reqres.in/api/';
+
+export type User = {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+};
+
+type UserData = {
+  data: User[];
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+};
+
+export async function getUsers(page: number) {
+  const response = await fetch(`${BASE_URL}users?page=${page}`);
+  const data = (await response.json()) as UserData;
+  return data;
+}
--- src/fragments/dashboard.ts
+++ src/fragments/dashboard.ts
@@ -1,6 +1,45 @@
+import { getUsers } from '../api/getUsers';
+
 export function renderDashboard() {
   const wrapper = document.querySelector<HTMLElement>('main')!;
   wrapper.innerHTML = /*html*/ `
-        <p>Dashboard!</p>
+        <p>Loading...</p>
     `;
+
+  const initialPage = 1;
+  displayUsers(initialPage);
+}
+
+async function displayUsers(page: number) {
+  const userData = await getUsers(page);
+  const users = userData.data;
+  const wrapper = document.querySelector<HTMLElement>('main')!;
+  wrapper.innerHTML = /*html*/ `
+    <table>
+      <caption>Users</caption>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Email</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${users
+            .map(
+              (user) =>
+                `<tr>
+            <td>${user.id}</td>
+            <td>${user.email}</td>
+            <td>${user.first_name}</td>
+            <td>${user.last_name}</td>
+            <td>${user.avatar}</td>
+          </tr>`
+            )
+            .join('')}
+        </tbody>
+    </table>  
+  `;
 }
