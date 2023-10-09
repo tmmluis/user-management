dc0ff43dc112652bd46192b89d2917a7c1a014c7
Tiago Luis <tmmluis@gmail.com>
2023-10-09 17:44:34 +0100

    improve styles

--- src/icons/check.ts
+++ src/icons/check.ts
@@ -1,4 +1,4 @@
-export const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+export const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="22" height="22" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
 `;
