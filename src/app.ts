8c1c311057ef400404097eaacb877108412849a4
tmmluis <tmmluis@gmail.com>
2023-10-24 12:19:02 +0200

    convert Footer to custom element

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import './style.css';
 import { renderHeader } from './fragments/header/header';
 import { renderMain } from './fragments/main';
-import { renderFooter } from './fragments/footer';
+import { Footer } from './fragments/Footer';
 import { renderLogin } from './fragments/login';
 import { renderDashboard } from './fragments/dashboard/dashboard';
 
 function renderAppLayout(root: HTMLDivElement) {
   renderHeader(root);
   renderMain(root);
-  renderFooter(root);
+  root.append(new Footer());
 }
 
 function startApp() {
---
+++ src/fragments/Footer.ts
@@ -0,0 +1,41 @@
+import { viteIcon } from '../icons/vite';
+import { gitHubIcon } from '../icons/gitHub';
+import { typescriptIcon } from '../icons/typescript';
+import { linkedinIcon } from '../icons/linkedin';
+import reqresIcon from '../icons/reqres.png';
+
+export class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = /*html*/ `
+      <footer>
+        <div id="tech-stack">
+          <p>Built with</p>
+          <a href="https://www.typescriptlang.org/">
+            ${typescriptIcon}
+          </a>
+            <a href="https://vitejs.dev/">
+              ${viteIcon}
+            </a>
+            <a href="https://reqres.in/">
+              <img src=${reqresIcon} alt="Reqres" title="Reqres">
+            </a>
+        </div>
+        <div id="social-media">
+          <p>Connect</p>
+          <a href="https://github.com/tmmluis">
+            ${gitHubIcon}
+          </a>
+          <a href="https://www.linkedin.com/in/tmmluis/">
+            ${linkedinIcon}
+          </a>
+        </div>
+      </footer>
+    `;
+  }
+}
+
+customElements.define('portfolio-footer', Footer);
